f1ead088578f735d711f00c89c1dfe4f55d6d12d
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-11-28 15:13:16 +0100

    Fixed formatting

--- library/Bav/EncoderInterface.php
+++ library/Bav/EncoderInterface.php
@@ -26,10 +26,8 @@ namespace Bav;
 
 interface EncoderInterface
 {
-    
     static public function isSupported($encoding);
     public function strlen($string);
     public function substr($string, $offset, $length = null);
     public function convert($string, $fromEncoding);
-    
 }
