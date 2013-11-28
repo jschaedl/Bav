64a6ef95ca9d00852ff8200159691458cd3e8aac
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-11-28 11:34:34 +0100

    Changed phpunit settings

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,5 @@
 <?php
 
-spl_autoload_register(include '..' . DIRECTORY_SEPARATOR . 'autoloader' . DIRECTORY_SEPARATOR . 'autoloader.php');
+// spl_autoload_register(include '..' . DIRECTORY_SEPARATOR . 'autoloader' . DIRECTORY_SEPARATOR . 'autoloader.php');
+
+require dirname(__FILE__) . '/../vendor/autoload.php';
