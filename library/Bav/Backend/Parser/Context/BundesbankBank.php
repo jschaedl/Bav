eb4428568b5eaff00896232f4ecf82d8982d94ab
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-11-29 10:48:55 +0100

    Fixed formatting

--- library/Bav/Backend/Parser/Context/BundesbankBank.php
+++ library/Bav/Backend/Parser/Context/BundesbankBank.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (C) 2012  Dennis Lassiter <dennis@lassiter.de>
+ * Copyright (C) 2012 Dennis Lassiter <dennis@lassiter.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -10,12 +10,12 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  *
  * @package Backend
@@ -23,84 +23,49 @@
  * @author Dennis Lassiter <dennis@lassiter.de>
  * @copyright Copyright (C) 2012 Dennis Lassiter
  */
-
 namespace Bav\Backend\Parser\Context;
 
 class BundesbankBank
 {
-    
+	private $line = 0;
+	private $start = null;
+	private $end = null;
 
-    private
-    /**
-     * @var int any line of the context
-     */
-    $line = 0,
-    /**
-     * @var int the first line in the context
-     */
-    $start = null,
-    /**
-     * @var int the last line of the context
-     */
-    $end = null;
-    
-    
-    /**
-     * @param string $bankID
-     * @param int $line
-     */
-    public function __construct($line) {
-        $this->line   = $line;
-    }
-    /**
-     * @return int
-     */
-    public function getLine() {
-        return $this->line;
-    }
-    /**
-     * @throws BAV_FileParserContextException_Undefined
-     * @return int
-     */
-    public function getStart() {
-        if (is_null($this->start)) {
-            throw new \Exception();
-        }
-        return $this->start;
-    }
-    /**
-     * @return bool
-     */
-    public function isStartDefined() {
-        return ! is_null($this->start);
-    }
-    /**
-     * @throws BAV_FileParserContextException_Undefined
-     * @return int
-     */
-    public function getEnd() {
-        if (is_null($this->end)) {
-            throw new \Exception();
-        
-        }
-        return $this->end;
-    }
-    /**
-     * @return bool
-     */
-    public function isEndDefined() {
-        return ! is_null($this->end);
-    }
-    /**
-     * @param int $start
-     */
-    public function setStart($start) {
-        $this->start = $start;
-    }
-    /**
-     * @param int $end
-     */
-    public function setEnd($end) {
-        $this->end = $end;
-    }
+	public function __construct($line) {
+		$this->line = $line;
+	}
+
+	public function getLine() {
+		return $this->line;
+	}
+
+	public function getStart() {
+		if (is_null($this->start)) {
+			throw new \Exception();
+		}
+		return $this->start;
+	}
+
+	public function getEnd() {
+		if (is_null($this->end)) {
+			throw new \Exception();
+		}
+		return $this->end;
+	}
+
+	public function setStart($start) {
+		$this->start = $start;
+	}
+
+	public function setEnd($end) {
+		$this->end = $end;
+	}
+
+	public function isStartDefined() {
+		return !is_null($this->start);
+	}
+
+	public function isEndDefined() {
+		return !is_null($this->end);
+	}
 }
