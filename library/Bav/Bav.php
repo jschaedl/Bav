cfd3952e871197e17bf3b2e720a226bfa6a579e8
Martins Sipenko <martins.sipenko@gmail.com>
2016-06-29 15:50:30 +0300

    Move data folder out from tests directory

--- library/Bav/Bav.php
+++ library/Bav/Bav.php
@@ -22,14 +22,14 @@ class Bav
 
     public static function DE()
     {
-        $bankDataFile = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'tests/data/blz_2014_09_08_txt.txt';
-        
+        $bankDataFile = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'data/blz_2014_09_08_txt.txt';
+
         $parser = new BankDataParser($bankDataFile);
         $parser->setEncoder(EncoderFactory::create(Bav::DEFAULT_ENCODING));
-        
+
         $bav = new Bav();
         $bav->setBankDataResolver(new BankDataResolver($parser));
-        
+
         return $bav;
     }
 
